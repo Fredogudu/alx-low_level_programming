cc85b77a5f0978a6a53aefe3d6f93e63e358ab84
fredogudu <fredogudu1@gmail.com>
2022-12-13 19:20:11 -0800

    task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * main - print _alphabet_x10 - print all alphabet in lowercase
+ * main - print _alphabet_x10 - print 10 time the alphabet, in lowercase
  */
 
-void print_alphabet(void);
+void print_alphabet_10(void);
 {
+	int count = 0;
 	char letter;
 
+	while (count++ <= 9)
+	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar("/n");
+	}
 }
